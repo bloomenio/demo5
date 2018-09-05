4c75ad54ca3c13b5b6d39fb20ce1be79833414db
Nikos Kapsoulis <nkapsoulis@gmail.com>
2018-09-05 15:29:49 +0300

    Ports

--- run-demo.sh
+++ run-demo.sh
@@ -5,18 +5,19 @@ set -e  # To exit on first error.
 if [ "$(docker ps -q)" > /dev/null ];then docker kill $(docker ps -q); fi
 if [ "$(docker ps -aq)" > /dev/null ];then docker rm $(docker ps -aq); fi
 # docker rmi $(docker images dev-* -q)
-# Shutdown any programs listening on ports 3000, 9090 of localhost.
-if [ "$(sudo lsof -ti :3000,9090)" > /dev/null ];then kill -9 $(sudo lsof -ti :3000,9090); fi
+# Shutdown any programs listening on ports 3000, 9090, of localhost.
+if [ "$(sudo lsof -ti :80,3000,9090)" > /dev/null ];then kill -9 $(sudo lsof -ti :80,3000,9090); fi
 
 echo "Running Anonymous Personalization Demo of plenary meeting in Cyprus, June 2018..."
-wd=$PWD
+pwd=$PWD
 cd webtv_back/
 if [ -f nohup.out ]; then rm nohup.out; fi
 nohup /bin/bash ./run.sh &
+sleep 1
 while [ "$(tail -1 nohup.out)" != "Browse your REST API at http://localhost:3000/explorer" ];do sleep 5; done
-nohup /bin/bash ./run-explorer.sh &
-while [ "$(tail -1 nohup.out)" != "Please open Internet explorer to access ：http://localhost:9090/" ];do sleep 5; done
-cd $wd/webtv_front/
+#nohup /bin/bash ./run-explorer.sh &
+#while [ "$(tail -1 nohup.out)" != "Please open Internet explorer to access ：http://localhost:9090/" ];do sleep 5; done
+cd $pwd/webtv_front/
 nohup /bin/bash input_data.sh &
 sleep 10
 /bin/bash run-front-end.sh &
